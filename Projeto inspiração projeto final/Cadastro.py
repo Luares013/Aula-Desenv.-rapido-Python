ff95f688c1d80aae6403343aad01d71dcf195bfb
LUCAS LUARES DE OLIVEIRA <Lucasfurao5@gmail.com>
2026-06-09 21:09:17 -0300

    Update Cadastro.py

--- Projeto inspiração projeto final/Cadastro.py
+++ Projeto inspiração projeto final/Cadastro.py
@@ -1,8 +1,8 @@
 from PyQt5 import uic,QtWidgets
 import pymysql.connections
 
-banco = pymysql.connections.Connetion(
-    host = "localhost:8080",
+banco = pymysql.connections.Connection(
+    host = "localhost",
     user = "root",
     passwd = "",
     database = "sistemacadastro"
@@ -14,25 +14,29 @@ def funcao_principal():
     cidade = formulario.lineEdit_3.text()
 
     cursor = banco.cursor()
-    sql = "INSERT INTO usuario(nome,telefone.cidade) VALUES (%s,%s,%s)"
+    sql = "INSERT INTO usuario(nome,telefone,cidade) VALUES (%s,%s,%s)"
     dados = (str(nome), str(telefone), str(cidade))
     cursor.execute(sql,dados)
     banco.commit()
     formulario.lineEdit.setText("")
+    formulario.lineEdit_2.setText("")
+    formulario.lineEdit_3.setText("")
 
 def listarDados():
     mostrarDados.show()
+
     cursor = banco.cursor()
+
     sql = "SELECT * FROM usuario"
     cursor.execute(sql)
     dados_lidos = cursor.fetchall()
-    mostrarDados.tableWidget.setRowCount(len(dados_lidos))
-    mostrarDados.tableWidget.setColunnCount(4)
 
-    for linha in range (0,len(dados_lidos)):
-        for coluna in range (0,4):
-            mostrarDados.tableWidget.setItem(linha, coluna, QtWidgets.
-            QTableWidgetItem(str(dados_lidos[linha][coluna])))
+    mostrarDados.tableWidget.setRowCount(len(dados_lidos))
+    mostrarDados.tableWidget.setColumnCount(4)
+
+    for linha in range(0, len(dados_lidos)):
+        for coluna in range(0, 4):
+            mostrarDados.tableWidget.setItem(linha, coluna, QtWidgets.QTableWidgetItem(str(dados_lidos[linha][coluna])))
 
 def excluirUsuario():
     linha = mostrarDados.tableWidget.currentRow()
@@ -44,16 +48,20 @@ def excluirUsuario():
     cursor.execute(sql, (id_usuario,))
     banco.commit()
 
-    print("Usuario excluido com sucesso")
-    
+    print("Usuário excluído com sucesso!")
+
     listarDados()
 
+    cursor.execute(sql, (id_usuario,))
+
+    
 app = QtWidgets.QApplication([])
+
 formulario = uic.loadUi("TelaDeCadastro.ui")
 mostrarDados = uic.loadUi("listar.ui")
-formulario.pushButton.clicked.connect(funcao_principal)
-formulario.pushButton_2.clicked.connect(listardados)
 
+formulario.pushButton.clicked.connect(funcao_principal)
+formulario.pushButton_2.clicked.connect(listarDados)
 
 formulario.show()
 app.exec()
